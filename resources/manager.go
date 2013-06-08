2c3be1f9a7eb32e2e98ae86539c3c266dcc5cdd3
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2013-06-08 11:03:24 +0100

    Moved to using external libsg and rewrote manager to load sg3 files

--- resources/manager.go
+++ resources/manager.go
@@ -2,201 +2,222 @@ package resources
 
 import (
 	"log"
-	"path"
-	"path/filepath"
-	_ "sync"
+	"fmt"
+	"math"
+	"sync"
 
+	libsg "github.com/bluepeppers/libsg/golibsg"
 	"github.com/bluepeppers/allegro"
 	"github.com/go-gl/gl"
-	"github.com/go-gl/glfw"
 )
 
 const (
-	DEFAULT_TILE_NAME   = "____DEFAULT____"
-	DEFAULT_TILE_WIDTH  = 128
-	DEFAULT_TILE_HEIGHT = 128
+	DEFAULT_GRAPHIC_NAME   = "____DEFAULT____"
+	DEFAULT_GRAPHIC_WIDTH  = 128
+	DEFAULT_GRAPHIC_HEIGHT = 128
 )
 
-// Our custom super special bitmap class
-type Bitmap struct {
+var DEFAULT_GRAPHIC_DATA uint32[];
+
+func init() {
+	DEFAULT_GRAPHIC_DATA = make(uint32[], DEFAULT_GRAPHIC_WIDTH * DEFAULT_GRAPHIC_HEIGHT)
+	for x := 0; x < DEFAULT_GRAPHIC_WIDTH; x++ {
+		for y := 0; y < DEFAULT_GRAPHIC_HEIGHT; y++ {
+			p := y * DEFAULT_GRAPHIC_WIDTH + x
+
+			// Something funky
+			fr := math.Cos(y/DEFAULT_GRAPHIC_WIDTH)
+			fg := math.Sin(x/DEFAULT_GRAPHIC_WIDTH)
+			fb := math.Tan(p/len(DEFAULT_GRAPHIC_DATA))
+
+			r := byte(fr * 255)
+			g := byte(fg * 255)
+			b := byte(fb * 255)
+			a := 255
+			DEFAULT_GRAPHIC_DATA[p] =
+				(r << 6) |
+				(g << 4) |
+				(b << 2) |
+				(a << 0)
+		}
+	}
+}
+
+// Our custom super special graphic class
+type Graphic struct {
 	Tex gl.Texture
-	// The offset of the bitmap when drawing
+	// The offset of the graphic when drawing
 	OffX, OffY int
 	// Dimensions
 	W, H int
 }
 
-// Infomation about a tile in the atlas
-type tileMetadata struct {
-	x, y, w, h int
-	offx, offy int
-	name       string
-}
-
 type ResourceManager struct {
-	tileMetadatas map[string]tileMetadata
-	tileBmps      map[string]Bitmap
+	tileGraphics map[string]Graphic
 
 	fontMap map[string]*allegro.Font
 }
 
-func CreateResourceManager(config *ResourceManagerConfig) *ResourceManager {
-	/*	defaultTileConfig := TileConfig{Name: DEFAULT_TILE_NAME, Filename: DEFAULT_TILE_NAME}
-		config.TileConfigs = append(config.TileConfigs, defaultTileConfig)
-
-		tileConfs := make([]TileConfig, 0)
-		for _, v := range config.TileConfigs {
-			tileConfs = append(tileConfs, v)
-		}
-
-		var manager ResourceManager
-
-		nBmps := len(tileConfs)
-		log.Printf("Loading %v bitmaps", nBmps)
-
-		loadedBmps := make([]*allegro.Bitmap, nBmps)
-		manager.tileMetadatas = make([]tileMetadata, nBmps)
-		var wg sync.WaitGroup
-		wg.Add(nBmps)
-		for j := 0; j < nBmps; j++ {
-			go func (i int) {
-				cfg := tileConfs[i]
-
-				// Load the bitmap
-				var bmp *allegro.Bitmap
-				if cfg.Name == DEFAULT_TILE_NAME {
-					bmp = allegro.NewBitmap(DEFAULT_TILE_WIDTH, DEFAULT_TILE_HEIGHT)
-				} else {
-					bmp = allegro.LoadBitmap(cfg.Filename)
-				}
-				if bmp == nil {
-					log.Fatalf("Failed to load bmp %v", cfg)
-				}
-
-				loadedBmps[i] = bmp
-				manager.tileMetadatas[i] = generateMetadata(bmp, cfg)
-
-				wg.Done()
-			} (j)
-		}
-		wg.Wait()
-		log.Printf("Loaded bitmaps")
-
-		manager.tileBmps = make([]*Bitmap, nBmps)
-		for i, bmp := range loadedBmps {
-			metadata := manager.tileMetadatas[i]
-			fBmp := &Bitmap{gl.Texture(bmp.GetGLTexture()), bmp,
-				metadata.offx, metadata.offy, metadata.w, metadata.h}
-			manager.tileBmps[i] = fBmp
-		}
-
-		manager.tilePos = make(map[string]int, nBmps)
-		for i, metadata := range manager.tileMetadatas {
-			manager.tilePos[metadata.name] = i
-		}*/
-
+func CreateResourceManager() *ResourceManager {
 	var manager ResourceManager
-	manager.tileMetadatas = make(map[string]tileMetadata)
-	manager.tileBmps = make(map[string]Bitmap)
 
-	// Load the fonts
+	manager.tileGraphics = make(map[string]Graphic)
+	manager.addDefaultGraphic()
+
 	manager.fontMap = make(map[string]*allegro.Font)
-	for _, v := range config.FontConfigs {
-		var font *allegro.Font
-		if v.Filename == "builtin" {
-			font = allegro.CreateBuiltinFont()
-		} else {
-			font = allegro.LoadFont(v.Filename, v.Size, 0)
-		}
-		manager.fontMap[v.Name] = font
-	}
+	manager.fontMap["builtin"] = allegro.CreateBuiltinFont()
 
 	return &manager
 }
 
-func (rm *ResourceManager) GetTile(name string) (*Bitmap, bool) {
-	bmp, ok := rm.tileBmps[name]
-	if !ok {
-		return rm.loadTile(name)
+func (rm *ResourceManager) addDefaultGraphic() {
+	var graphic Graphic
+	allegro.RunInThread(func() {
+		graphic.Tex = gl.GenTexture()
+		graphic.Tex.Bind(gl.TEXTURE_2D)
+
+		// Maybe REPEAT will be more noticable?
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+		
+		// Why not
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+
+		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA32,
+			DEFAULT_GRAPHIC_WIDTH, DEFAULT_GRAPHIC_HEIGHT,
+			0, gl.RGBA32, DEFAULT_GRAPHIC_DATA)
+	})
+	graphic.Width = DEFAULT_GRAPHIC_WIDTH
+	graphic.Height = DEFAULT_GRAPHIC_HEIGHT
+	
+	rm.tileGraphics[DEFAULT_GRAPHIC_NAME] = graphic
+}
+
+func (rm *ResourceManager) LoadSG3File(filenameSG3, filename555, prefix string) error {
+	file, err := libsg.ReadFile(filename)
+	if err {
+		return err
 	}
-	return &bmp, true
-}
-
-// Gets a tile that can be drawn, no matter what. Won't be pretty, but won't crash.
-func (rm *ResourceManager) GetDefaultTile() *Bitmap {
-	sub, ok := rm.GetTile(DEFAULT_TILE_NAME)
-	if !ok {
-		log.Panicf("Could not find default key in atlas: %v", DEFAULT_TILE_NAME)
+	imgs, err := file.Images()
+	if err != nil {
+		return err
 	}
-	return sub
-}
 
-func (rm *ResourceManager) GetTileOrDefault(name string) *Bitmap {
-	tile, ok := rm.GetTile(name)
-	if !ok {
-		log.Printf("Could not find tile named %q. Defaulting to default tile.", name)
-		return rm.GetDefaultTile()
+	var wg sync.WaitGroup
+	wg.Add(len(imgs) + 1)
+
+	errChan := make(chan error)
+
+	go func() {
+		err = nil
+		err <- errChan
+		wg.Done()
+	} ()
+
+	for _, img := range imgs {
+		dataFile := filenameSG3
+		if img.IsExtern() {
+			dataFile = filename555
+		}
+		go func () {
+			err := rm.loadImage(img, name, dataFile)
+			if err != nil {
+				// Non blocking send
+				select {
+				case errChan <- err:
+				default:
+				}
+			}
+			wg.Done()
+		} ()
 	}
-	return tile
+	wg.Wait()
+	return err
 }
 
-func (rm *ResourceManager) GetFont(name string) (*allegro.Font, bool) {
-	font, ok := rm.fontMap[name]
-	return font, ok && font != nil
+func (rm *ResourceManager) loadImage(img *libsg.Image, name, dataFile string) error {
+	bmpName := strings.TrimSuffix(bmp.Filename(), ".bmp")
+	name := fmt.Sprintf("%v.%v.%v", prefix, bmpName, img.ID())
+	
+	imgData := img.LoadData(dataFile)
+	tex, err := img.loadTexture(imgData)
+	if err != nil {
+		return err
+	}
+	var graphic Graphic
+	graphic.Tex = tex
+	graphic.W = imgData.Width
+	graphic.H = imgData.Height
+
+	rm.tileGraphics[name] = graphic
+	return nil
 }
 
-func (rm *ResourceManager) loadTile(name string) (*Bitmap, bool) {
-	fname, _ := filepath.Abs(path.Join("resources", name))
+func (rm *ResourceManager) loadTexture(imgData *libsg.SgImageData) (gl.Texture, error) {
+	// We only support ARGB32 from libsg (convert to RGBA later)
+	if imgData.BMask != 0xff ||
+		imgData.GMask != 0xff00 ||
+		imgData.RMask != 0xff0000 ||
+		imgData.AMask != 0xff000000 {
+		return gl.Texture(0), fmt.Errorf("Unsupport image format for %q", name)
+	}
+
+	// Convert the argb to rgba
+	for i := range imgData {
+		imgData[i] = (imgData[i] & (~imgData.AMask)) << 2 | (imgData[i] & imgData.AMask) >> 6
+	}
+
 	var tex gl.Texture
 	allegro.RunInThread(func() {
 		tex = gl.GenTexture()
 		tex.Bind(gl.TEXTURE_2D)
-		glfw.LoadTexture2D(fname, 0)
+
+		// Why not
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+		
+		// To keep pixely art, GL_NEAREST
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+
+		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA32,
+			imgData.Width, imgData.Height,
+			0, gl.RGBA32, imgData.Data)
 	})
-	bmp := Bitmap{tex, 0, 0, DEFAULT_TILE_WIDTH, DEFAULT_TILE_HEIGHT}
-	rm.tileBmps[name] = bmp
-	return &bmp, true
+	return tex, nil
 }
 
-func generateMetadata(bmp *allegro.Bitmap, cfg TileConfig) tileMetadata {
-	// Load the metadata, and then sanitize it
-	x := cfg.X
-	y := cfg.Y
-	w := cfg.W
-	h := cfg.H
-	ox := cfg.OffX
-	oy := cfg.OffY
-	bmpw, bmph := bmp.GetDimensions()
-	if bmpw < x {
-		x = 0
-		w = bmpw
-	} else if bmpw < x+cfg.W {
-		w = bmpw - x
-	} else {
-		w = cfg.W
+func (rm *ResourceManager) GetGraphic(name string) (Graphic, error) {
+	graphic, ok := rm.tileGraphics[name]
+	if !ok {
+		return Graphic{}, fmt.Errorf("Could not find graphic %q", name)
 	}
-	if bmph < y {
-		y = 0
-		h = bmph
-	} else if bmph < y+cfg.H {
-		h = bmph - y
-	} else {
-		h = cfg.H
-	}
+	return &bmp, nil
+}
 
-	if w == 0 {
-		w = bmpw - x
+// Gets a tile that can be drawn, no matter what. Won't be pretty, but won't crash.
+func (rm *ResourceManager) GetDefaultGraphic() *Bitmap {
+	sub, ok := rm.GetGraphic(DEFAULT_GRAPHIC_NAME)
+	if !ok {
+		log.Panicf("Could not find default graphic %q", DEFAULT_TILE_NAME)
 	}
-	if h == 0 {
-		h = bmph - y
-	}
+	return sub
+}
 
-	if ox > w {
-		ox = 0
+func (rm *ResourceManager) GetGraphicOrDefault(name string) *Bitmap {
+	graphic, ok := rm.GetGraphic(name)
+	if !ok {
+		log.Printf("Could not find graphic named %q. Defaulting to default graphic.", name)
+		return rm.GetDefaultTile()
 	}
-	if oy > h {
-		oy = 0
-	}
+	return graphic
+}
 
-	return tileMetadata{x, y, w, h, ox, oy, cfg.Name}
+func (rm *ResourceManager) GetFont(name string) (*allegro.Font, error) {
+	font, ok := rm.fontMap[name]
+	if !ok {
+		return nil, fmt.Errorf("Could not find font %q", name)
+	}
+	return font, nil
 }
